d66fb10fd1b92503f7ee2dcdfa92755244128d7f
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-11-30 17:22:50 +0530

    Update Application.java

--- src/main/java/SpringBootCRUD/Spring/Application.java
+++ src/main/java/SpringBootCRUD/Spring/Application.java
@@ -11,7 +11,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	@GetMapping(value = {"/hello")
+	@GetMapping("/hello")
     public String print() {
         return "Hello World!";
     }
