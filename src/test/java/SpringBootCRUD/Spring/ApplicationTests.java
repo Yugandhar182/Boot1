1f0d61d26357620f050a784a8c0bcd14e6510c10
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-11-30 17:04:27 +0530

    Add files via upload

---
+++ src/test/java/SpringBootCRUD/Spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootCRUD.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
